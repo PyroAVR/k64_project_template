97198392b3cfbd8c748ba88d0d47c634883fd64b
Seth Gower <seth@sethgower.com>
2020-12-31 18:17:31 -0500

    updates the flash script to allow argument binary (#7)

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,5 +2,9 @@
 WHERE=$(dirname $0)
 source $WHERE/openocd_server.sh
 start_oocd
-(echo "init;kinetis mdm mass_erase 0;reset halt;flash write_image build/main.bin 0 bin;reset run; exit"; sleep 1; echo "quit")| telnet localhost 4444
+if [[ -n $1 ]]; then
+    (echo "init;kinetis mdm mass_erase 0;reset halt;flash write_image $1 0 bin;reset run; exit"; sleep 1; echo "quit")| telnet localhost 4444
+else
+    (echo "init;kinetis mdm mass_erase 0;reset halt;flash write_image $(pwd)/build/main.bin 0 bin;reset run; exit"; sleep 1; echo "quit")| telnet localhost 4444
+fi
 
